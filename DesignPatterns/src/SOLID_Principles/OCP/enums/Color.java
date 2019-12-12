90517a696875ee72a711789b0085b143a5a29c78
Amr Eshra <amr.eshra@gmail.com>
2019-12-13 01:15:42 +0300

    OCP

---
+++ DesignPatterns/src/SOLID_Principles/OCP/enums/Color.java
@@ -0,0 +1,6 @@
+package SOLID_Principles.OCP.enums;
+
+public enum Color
+{
+  RED, GREEN, BLUE
+}
---
+++ DesignPatterns/src/SOLID_Principles/OCP/enums/Size.java
@@ -0,0 +1,6 @@
+package SOLID_Principles.OCP.enums;
+
+public enum Size
+{
+  SMALL, MEDIUM, LARGE, YUGE
+}
