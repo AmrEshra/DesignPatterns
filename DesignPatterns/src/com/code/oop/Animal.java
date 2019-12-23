fbaae4f8e138319408e97075c1d64c6347095e18
Amr Eshra <amr.eshra@gmail.com>
2019-12-23 10:18:55 +0300

    oop

---
+++ DesignPatterns/src/com/code/oop/Animal.java
@@ -0,0 +1,6 @@
+package com.code.oop;
+
+abstract class Animal {
+	// abstract method
+	public abstract void sound();
+}
---
+++ DesignPatterns/src/com/code/oop/Dog.java
@@ -0,0 +1,13 @@
+package com.code.oop;
+
+public class Dog extends Animal {
+
+	public void sound() {
+		System.out.println("Woof");
+	}
+
+	public static void main(String args[]) {
+		Animal obj = new Dog();
+		obj.sound();
+	}
+}
