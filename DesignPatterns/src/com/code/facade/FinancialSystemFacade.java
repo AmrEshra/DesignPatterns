55a869821397918743de820edfcc499916705227
Amr Eshra <amr.eshra@gmail.com>
2019-12-30 13:56:22 +0300

    facade

---
+++ DesignPatterns/src/com/code/facade/FinancialSystemFacade.java
@@ -0,0 +1,22 @@
+package com.code.facade;
+
+public class FinancialSystemFacade {
+
+    private BillingSystem billingSystem;
+
+    private InvoiceCustomerSystem invoiceCustomerSystem;
+
+    public void createInvoice(Integer amount){
+
+        Bill bill = billingSystem.createBill(amount);
+        invoiceCustomerSystem.createInvoiceForBill(bill);
+    }
+
+    public void setBillingSystem(BillingSystem billingSystem) {
+        this.billingSystem = billingSystem;
+    }
+
+    public void setInvoiceCustomerSystem(InvoiceCustomerSystem invoiceCustomerSystem) {
+        this.invoiceCustomerSystem = invoiceCustomerSystem;
+    }
+}
