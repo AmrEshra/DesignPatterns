24e362a00188072071450d2e70a08a31a106f6a5
Amr Eshra <amr.eshra@gmail.com>
2020-01-06 14:53:27 +0300

    chainofresponsibility

---
+++ DesignPatterns/src/com/code/chainofresponsibility/Client.java
@@ -0,0 +1,15 @@
+package com.code.chainofresponsibility;
+
+public class Client {
+
+    public static void main(String[] args) {
+
+        PaymentCheckoutHandler handler = new PaymentCheckoutHandler();
+        CustomerInfoCheckoutHandler customerInfoCheckoutHandler = new CustomerInfoCheckoutHandler();
+
+        handler.setNextHandler(customerInfoCheckoutHandler);
+
+        handler.handlCheckout();
+    }
+
+}
---
+++ DesignPatterns/src/com/code/chainofresponsibility/CustomerInfoCheckoutHandler.java
@@ -0,0 +1,12 @@
+package com.code.chainofresponsibility;
+
+public class CustomerInfoCheckoutHandler extends CheckoutHandler{
+    @Override
+    public void handlCheckout() {
+        System.out.println("Adding extra info to checkout of the customer");
+
+        if(nextHandler != null){
+            nextHandler.handlCheckout();
+        }
+    }
+}
